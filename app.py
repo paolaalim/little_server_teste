73e65aef4dded3e18b1aacf7081cd061b19b5e74
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2025-05-29 15:14:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return "Olá, Easypanel!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # usa a porta 5000 se PORT não for definida
+    app.run(host="0.0.0.0", port=port)
